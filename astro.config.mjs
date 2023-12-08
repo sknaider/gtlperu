a6a46565ccb4234820ddf059f81c35f082eb8080
William Tovar <32226031+sknaider@users.noreply.github.com>
2023-12-08 18:51:16 -0500

    first second

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
+
 import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship-pro.web3templates.com",
   image: {
     service: sharpImageService(),
-    domains: ["source.unsplash.com", "images.unsplash.com"],
+    domains: ["source.unsplash.com", "images.unsplash.com"]
   },
   integrations: [tailwind(), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
